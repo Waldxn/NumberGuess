bb571c2bbaffef65da31b213765649c5a755e78d
Waldxn <chriswalden80@gmail.com>
2017-07-12 20:35:32 -0500

    Updates

--- script.py
+++ script.py
@@ -2,41 +2,52 @@ from random import random
 
 
 class Game:
-    """
-    rand is declared by grabbing a number between 0 and 1, multiplying it by 100, and rounding to the nearest integer
-    guessed is declared as false in order to keep the while loop running until the number is guessed
-    """
-    rand = round(random() * 100, 0)
-    guessed = False
-    print("Guess the number [1 - 100]")
 
     # This function handles the number guessing and number formatting
     def run_game(self):
 
-        # Assigns the 'answer' variable by grabbing user input from console
-        answer = input()
+        # rand is declared by grabbing a number between 0 and 1, multiplying it by 100, and rounds to nearest integer
+        rand = round(random() * 100, 0)
+        print("Guess the number [0 - 100]")
+        guesses = 0
 
-        # Checks if the input from the console is a number, and if not, asks the user to enter a valid number
-        if answer.isdigit():
+        while True:
 
-            n = int(answer)
+            # Assigns the 'answer' variable by grabbing user input from console
+            answer = input()
 
-            # Checks the input given against the random number generated
-            while not self.guessed:
-                if n > int(self.rand):
+            # Checks if the input from the console is a number, and if not, asks the user to enter a valid number
+            if answer.isdigit():
+                n = int(answer)
+                if n > int(rand):
                     print("Number is less than " + str(n))
-                    self.run_game()
-                elif n < int(self.rand):
+                    guesses = guesses + 1
+                elif n < int(rand):
                     print("Number is greater than " + str(n))
-                    self.run_game()
+                    guesses = guesses + 1
                 else:
-                    print("You have guessed the correct number!")
-                    self.guessed = True
-                    return self.guessed
-        else:
-            print("Please enter a number")
-            self.run_game()
-            return
+                    guesses = guesses + 1
+                    print("It took you " + str(guesses) + " guesses to guess the right number!")
+                    reply = self.play_again()
+                    if reply is False:
+                        break
+                    else:
+                        self.run_game()
+            else:
+                print("Please enter a number")
+
+    @staticmethod
+    def play_again():
+
+        while True:
+            reply = input("Play again? (y/n)")
+            if reply.lower() == "y":
+                return True
+            elif reply.lower() == "n":
+                return False
+            else:
+                print("Enter 'y' or 'n'")
+
 
 if __name__ == "__main__":
     game = Game()
