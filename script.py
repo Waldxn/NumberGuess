83d5af9be2535f7a222ae608563552a1da697264
Waldxn <chriswalden80@gmail.com>
2017-07-12 19:24:23 -0500

    Added comments for readability

--- script.py
+++ script.py
@@ -2,19 +2,26 @@ from random import random
 
 
 class Game:
+    """
+    rand is declared by grabbing a number between 0 and 1, multiplying it by 100, and rounding to the nearest integer
+    guessed is declared as false in order to keep the while loop running until the number is guessed
+    """
     rand = round(random() * 100, 0)
     guessed = False
-    number_guessed = 0
     print("Guess the number [1 - 100]")
 
+    # This function handles the number guessing and number formatting
     def run_game(self):
 
+        # Assigns the 'answer' variable by grabbing user input from console
         answer = input()
 
+        # Checks if the input from the console is a number, and if not, asks the user to enter a valid number
         if answer.isdigit():
-            self.number_guessed = int(answer)
-            n = int(self.number_guessed)
 
+            n = int(answer)
+
+            # Checks the input given against the random number generated
             while not self.guessed:
                 if n > int(self.rand):
                     print("Number is less than " + str(n))
